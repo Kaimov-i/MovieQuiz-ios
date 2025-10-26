29071d888b448cbb8e8b7614f44465cd65dd6ed9
Ильман <kaimov-ilman@yandex.ru>
2025-10-27 02:09:58 +0300

    Finish

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -7,20 +7,14 @@ struct QuizQuestion {
 }
 
 struct QuizStepViewModel {
-    // картинка с афишей фильма с типом UIImage
     let image: UIImage
-    // вопрос о рейтинге квиза
     let question: String
-    // строка с порядковым номером этого вопроса (ex. "1/10")
     let questionNumber: String
 }
 
 struct QuizResultsViewModel {
-  // строка с заголовком алерта
   let title: String
-  // строка с текстом о количестве набранных очков
   let text: String
-  // текст для кнопки алерта
   let buttonText: String
 }
 
