d159034a455aec31743f077c5e5e9b64adbc0443
Ильман <kaimov-ilman@yandex.ru>
2025-12-05 08:36:36 +0300

    Start sprint 7

---
+++ MovieQuizTests/MovieQuizTests.swift
@@ -0,0 +1,16 @@
+//
+//  MovieQuizTests.swift
+//  MovieQuizTests
+//
+//  Created by Ilman on 05.12.2025.
+//
+
+import Testing
+
+struct MovieQuizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
