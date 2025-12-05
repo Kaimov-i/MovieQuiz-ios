62d90d0064922e0e27b3ea5e7626c57085cae27f
Ильман <kaimov-ilman@yandex.ru>
2025-12-06 01:48:08 +0300

    Added MovieQuizTests class

--- MovieQuizTests/MovieQuizTests.swift
+++ MovieQuizTests/MovieQuizTests.swift
@@ -5,12 +5,34 @@
 //  Created by Ilman on 05.12.2025.
 //
 
-import Testing
+import XCTest
 
-struct MovieQuizTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+struct ArithmeticOperations {
+    func addition(num1: Int, num2: Int) -> Int {
+        num1 + num2
     }
+    
+    func subtracttion(num1: Int, num2: Int) -> Int {
+        num1 - num2
+    }
+    
+    func multiplication(num1: Int, num2: Int) -> Int {
+        num1 * num2
+    }
+}
 
+class MovieQuizTests: XCTestCase {
+    func testAddition() throws {
+        //Give
+        let arethmeticOperations = ArithmeticOperations()
+        let num1 = 1
+        let num2 = 2
+        
+        //When
+        
+        let result = arethmeticOperations.addition(num1: 1, num2: 3)
+        
+        //Then
+        XCTAssertEqual(result, 3)
+    }
 }
